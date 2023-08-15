267d73880954891ca2e9be620ac98d978930f39e
jtabordaj <jtabordaj@uninorte.edu.co>
2023-08-15 15:42:43 -0500

    updated readme, unfortunately this no longer works

--- eleuthia.R
+++ eleuthia.R
@@ -1,17 +1,17 @@
-## CODENAME: ELEUTHIA ##
+# NO LONGER WORKS
 # START OF FILE
 source('./functions.R')
 # auths
-api_key <- "SCBRcB3yXBX7DQv5PiQyBBeAW"
-api_secret_key <- "lfqh7IULSZrVrjW3OqO6QFdI0d9WtEpogx7HqWhXHOL5zTh7C0"
-access_token <- "1359353656559214599-9Vo0fjbRsdLv9MyZNHx29RDMcR8yOl"
-access_token_secret <- "H76PEEOwOCy3Qgl8MwKeBoB4VXuRJhBT2nQ1zZrPry1at"
+api_key <- ""
+api_secret_key <- ""
+access_token <- ""
+access_token_secret <- ""
 token <- doToken()
 
 # grab tweet datasets and clean it
 main <- rbind(
-    grabTweets(2500, "Ultra Air"), 
-    grabTweets(2500, "Viva Air")
+    grabTweets(2500, "Query"), 
+    grabTweets(2500, "Query")
 )
 main <- main %>% select(any_of(varNames))
 main <- main %>% mutate(text = tolower(text))
@@ -39,4 +39,3 @@ hist(afinnedTweets$sentimentScore)
 # may be of interest: https://shiny.rstudio.com/articles/basics.html
 
 # END OF FILE
-## CODENAME: ELEUTHIA ##
